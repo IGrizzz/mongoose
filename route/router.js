25071d1fbf4b962167121391929ba577374e6400
IGrizzz <muda3332@gmail.com>
2021-12-25 17:35:27 +0700

    api done

---
+++ route/router.js
@@ -0,0 +1,36 @@
+const express = require('express')
+
+const ParticipantsController = require("../controllers/participant.controller")
+const InstructorController = require("../controllers/instructor.controller")
+const CoursesController = require("../controllers/courses.controller")
+
+
+const router = express.Router()  
+
+//Participants
+
+router.post('/participant', ParticipantsController.createNewPar)
+router.get('/participant', ParticipantsController.getPar)
+router.get('/participant/:id', ParticipantsController.getParById)
+router.patch('/participants/:id', ParticipantsController.updatePar)
+router.delete('/particpants/:id', ParticipantsController.deletePar)
+
+//Instructor
+
+router.post('/instructor', InstructorController.createNewIns)
+router.get('/instructor', InstructorController.getIns)
+router.get('/instructor/:id', InstructorController.getInsById)
+router.patch('/instructor/:id', InstructorController.updateIns)
+router.delete('/instructor/:id', InstructorController.deleteIns)
+
+//Courses
+
+router.post('/courses', CoursesController.createNewCour)
+router.get('/courses', CoursesController.getCour)
+router.get('/courses/:id', CoursesController.getCourById)
+router.patch('/courses/:id', CoursesController.updateCour)
+router.delete('/courses/:id', CoursesController.deleteCour)
+
+
+
+module.exports = router
